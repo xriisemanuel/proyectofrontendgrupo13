050c31936c941885d5731257dc4e86c8ba4c5524
xriis25 <40331096@fi.unju.edu.ar>
2025-07-11 12:47:18 -0300

    configurando JSON

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -3,6 +3,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server // Cambiado de All a Server, valor válido según Angular SSR
   }
 ];
