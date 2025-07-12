e1ad85f49fbdc4b8ab69083b177ceb3f511dfd6f
xriis25 <40331096@fi.unju.edu.ar>
2025-07-11 21:30:11 -0300

    nuevos cambios para subir al RENDER

--- frontend/src/app/core/constants/constants.ts
+++ frontend/src/app/core/constants/constants.ts
@@ -1,4 +1,8 @@
 // src/app/core/constants.ts
 // this file works as a central place to define constants used across the application, in this case, the API base URL
 // It can be imported in any component or service that needs to make API calls
-export const API_BASE_URL = 'http://localhost:3000/api';
+
+import { environment } from '../../../environments/environment';
+
+// Usar la configuración del environment
+export const API_BASE_URL = environment.apiUrl;
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://tu-backend-app.onrender.com/api'
+}; 
